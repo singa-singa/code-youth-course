0c05c733234976a1f98476b5dc06a481034858ac
singa-singa <singa.sinagamalu@gmail.com>
2022-07-08 13:49:50 -0400

    yes

---
+++ java.html/arrey.js
@@ -0,0 +1,13 @@
+let a = 4;
+let arr1 = [2, 4, 5, 6, 2, 8, 9];
+let arr2 = ["red", "green", "blue", "black"];
+let arr3 = ["hello", ture, 12.5, 78, false, "john"];
+console.log("arrey size", arrr1.length);
+console.log("arrey size", arr2.length);
+console.log("arrey size", arr3.length);
+
+
+for (let i = 0; i < arr1.length; i++) {
+    console.log(arr1[i] * arr1[i])
+    if (arr1[1])
+}
